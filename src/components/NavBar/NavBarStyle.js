a8fac95b06528907dcb5be5c0f4e7d6a1e810399
Luca Belotti <belotti38183@estudiantes.untref.edu.ar>
2021-06-02 18:12:30 -0300

    navbar con botones y styles por separado

--- src/components/NavBar/NavBarStyle.js
+++ src/components/NavBar/NavBarStyle.js
@@ -1,2 +1,62 @@
+import React from 'react';
+import {makeStyes} from '@material-ui/core';
 
+
+
+
+const commonStyles = {
+    display: 'flex',
+    alignSelf: 'center',
+
+};
+
+export const navBarStyle = theme => {
+    return({
+    
+    
+    navStyle: {
+    display: 'flex',
+    background: 'linear-gradient(45deg, black 70%,#7D0C32 90%)',
+    
+    
+  },
+  leftContainer:{
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'rigth',
+    columnGap: '2em',
+      },
+    brand: {
+      
+      color : '#FFFCFC',
+      fontFamily: 'roboto',
+      fontWeight: 'bold',
+      alignSelf: 'normal',
+      '@media (max-width:600px)':{
+        alignSelf: 'center',
+        
+    },
+      
+    },
+
+  buttonStyle:{
+      display: 'flex',
+      alignSelf: 'center',
+      flexDirection: 'row',      
+     
+      color: '#E4E8FE',
+      flexGrow: 1,
+      '&:hover':{ background: 'black',
+      color: '#FFFCFC',
+      }
+  },
+      centerConteiner: {
+       display: 'flex',
+       columnGap: '1em'
+},
+  
+      
+      
+  
+})}; 
 
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,51 +1,16 @@
 import React from 'react';
 import {AppBar, Toolbar, Typography, makeStyles,
     ListItem, ListItemText, List, Button} from '@material-ui/core';
-//import {NavBarStyle} from './NavBarStyle.js';
-   
-const useStyles = makeStyles ((theme) => ({
-    root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  navStyle: {
-    background: 'linear-gradient(45deg, black 30%, #D6540F 90%)',
+import {navBarStyle} from './NavBarStyle.js';
+
+const commonStyles = {
+    display: 'flex',
+    alignSelf: 'center',
+
+}   
+const useStyles = makeStyles ((theme) => navBarStyle (theme));
     
-  },
-  leftContainer:{
-      
-
-  },
-
-  brand: {
-      color : '#FFFCFC',
-      fontFamily: 'roboto',
-    },
-  list : {
-
-  },
-  menuText : {
-      alignContent : 'right'
-      
-  },
-  buttonStyle:{
-      background:'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      color: '#FFFCFC',
-      flexGrow: 1,
-      alignSelf: 'right',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
-  },
-      
-      
-  
-})); 
+   
 
 export const NavBar = props => {
         
@@ -57,22 +22,14 @@ export const NavBar = props => {
                 <Typography variant = "h5" className = {classes.brand}>
                    Samplecity 
                 </Typography>
-                <List className = {classes.list}>
-                    <ListItem button> 
-                    <ListItemText className = {classes.menuText} primary = {'Sonidos'} />
-                    
-                    </ListItem>
-                   
-                </List>
+                <div className = {classes.centerConteiner}>
                 <Button className = {classes.buttonStyle}>sonidos</Button>
                 <Button className = {classes.buttonStyle}>iniciar sesion</Button>
+                </div> 
+                
                     </div>
-
-        </Toolbar>
-        
-
-
-    </AppBar>
+                    </Toolbar>
+         </AppBar>
 
     };
 
